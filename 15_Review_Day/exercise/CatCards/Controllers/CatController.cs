d1b32d7ced806d62840388622921f68e4382e841
John Fulton <john@techelevator.com>
2022-03-03 16:16:51 -0500

    week 7 pairs exercise/

---
+++ 15_Review_Day/exercise/CatCards/Controllers/CatController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CatCards.DAO;
+using CatCards.Models;
+using CatCards.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatCards.Controllers
+{
+
+    public class CatController : ControllerBase
+    {
+        private readonly ICatCardDao cardDao;
+        private readonly ICatFactService catFactService;
+        private readonly ICatPicService catPicService;
+
+        public CatController(ICatCardDao cardDao, ICatFactService catFact, ICatPicService catPic)
+        {
+            this.catFactService = catFact;
+            this.catPicService = catPic;
+            this.cardDao = cardDao;
+        }
+    }
+}
---
+++ mygit.sh
@@ -0,0 +1,5 @@
+git add -A
+git commit -m "week 7 pairs exercise/"
+git pull origin main
+git push origin main
+echo "done"
